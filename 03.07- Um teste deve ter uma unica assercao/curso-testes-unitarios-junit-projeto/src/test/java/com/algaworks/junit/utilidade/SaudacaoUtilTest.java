7a740d802f542cb80b397e55e6ff57b75f34bab7
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-08 12:24:23 -0300

    codigo-fonte da aula

---
+++ 03.07- Um teste deve ter uma unica assercao/curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,78 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@DisplayName("Testes unitarios Saudacao") // Adiciona descrição no nome dos Testes para melhor organiza-los
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class SaudacaoUtilTest {
+
+    @Test
+    @DisplayName("Deve saldar com Bom Dia")
+    void saudarBomDiaTest() {
+        // Arrange -> Preparação do cenário: Variaveis que temos que declarar
+        String mensagem = "Bom dia";
+
+        // Act
+        String saudacao = SaudacaoUtil.saudar(9);
+
+        // Assert
+        assertEquals(saudacao, mensagem, "Saudacao incorreta");
+    }
+
+    @Test
+    void saudarBoaTardeTest() {
+        String mensagem = "Boa tarde";
+        String saudacao = SaudacaoUtil.saudar(14);
+
+        assertEquals(saudacao, mensagem);
+    }
+
+    @Test
+    void saudarBoaNoiteTest() {
+        String mensagem = "Boa noite";
+        String saudacao = SaudacaoUtil.saudar(4);
+
+        assertEquals(saudacao, mensagem);
+    }
+
+    @Test
+    void saudarBomDiaAPartir5HTest() {
+        String mensagem = "Bom dia";
+        String saudacao = SaudacaoUtil.saudar(5);
+
+        assertEquals(saudacao, mensagem, "Saudacao incorreta");
+    }
+
+    @Test
+    void saudarBoaNoiteAs4HTest() {
+        String mensagem = "Boa noite";
+        String saudacao = SaudacaoUtil.saudar(4);
+
+        assertEquals(saudacao, mensagem);
+    }
+
+    @Test
+    void dado_uma_hora_invalida_quando_saudar_entao_nao_deve_lancar_exception() {
+        int horaInvalida = -10;
+
+        Executable chamadaDeMetodoValida = () -> SaudacaoUtil.saudar(horaInvalida);
+
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, chamadaDeMetodoValida);
+
+        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void naoDeveLancarException() {
+        int horaIgalAZero = 0;
+        Executable chamadaaoMetodo = () -> SaudacaoUtil.saudar(horaIgalAZero);
+        assertDoesNotThrow(chamadaaoMetodo);
+    }
+
+}

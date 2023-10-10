7949683b8185df33032515941fc3e601588ad208
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-10 13:09:40 -0300

    codigo-fonte da aula

--- 04.04- Mock usando annotations/curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ 04.04- Mock usando annotations/curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -4,27 +4,37 @@ package com.algaworks.junit.blog.negocio;
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
 import com.algaworks.junit.blog.modelo.Editor;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@ExtendWith(MockitoExtension.class) // Para funcionar os @Mock
 class CadastroEditorComMockTest {
 
+    @InjectMocks
     CadastroEditor cadastroEditor;
     Editor editor;
+
+    @Mock
+    ArmazenamentoEditor armazenamentoEditor;
+    @Mock
+    GerenciadorEnvioEmail gerenciadorEnvioEmail;
 
     @BeforeEach
     void beforeEach() {
         editor = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
 
-        ArmazenamentoEditor armazenamentoEditor = Mockito.mock(ArmazenamentoEditor.class);
-        GerenciadorEnvioEmail gerenciadorDeEmail = Mockito.mock(GerenciadorEnvioEmail.class);
+//        ArmazenamentoEditor armazenamentoEditor = Mockito.mock(ArmazenamentoEditor.class);
+//        GerenciadorEnvioEmail gerenciadorDeEmail = Mockito.mock(GerenciadorEnvioEmail.class);
 
         Mockito.when(armazenamentoEditor.salvar(editor))
                 .thenReturn(new Editor(1L, "Alex", "alex@email.com", BigDecimal.TEN, true));
-
-        cadastroEditor = new CadastroEditor(armazenamentoEditor, gerenciadorDeEmail);
+        
     }
 
     @Test

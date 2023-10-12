14f0950ab37164118590df6a7437bc3fa988182a
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-12 19:49:51 -0300

    codigo-fonte da aula

---
+++ 04.12- Verificando ordem de chamada de metodos/curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -0,0 +1,160 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaBancariaTest {
+
+    //TODO - Testar o construtor com saldo igual a null OK
+    //TODO - Testar o saque com sucesso OK
+    //TODO - Testar o saque com valor igual a null OK
+    //TODO - Testar o saque com valor menor que zero OK
+    //TODO - Testar o saque com valor igual zero Ok
+
+    //TODO - Testar o deposito com sucesso OK
+    //TODO - Testar o deposito com valor igual a null OK
+    //TODO - Testar o deposito com valor menor que zero OK
+    //TODO - Testar o deposito com valor igual zero OK
+
+    //TODO - Testar retorno do valor do saldo OK
+
+    @Nested
+    class Saque {
+        /*
+    @Test
+    void saqueComSucesso() {
+        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+        contaBancaria.saque(BigDecimal.valueOf(25));
+
+        assertEquals(new BigDecimal("25"), contaBancaria.saldo());
+    }
+
+     */
+
+        @Test
+        void saqueComSucesso() {
+
+            BigDecimal saldoEmConta = BigDecimal.valueOf(50);
+            BigDecimal valorASerSacadoDaConta = BigDecimal.valueOf(25);
+
+            ContaBancaria contaBancaria = new ContaBancaria(saldoEmConta);
+            contaBancaria.saque(valorASerSacadoDaConta);
+
+            assertEquals(new BigDecimal("25"), contaBancaria.saldo());
+        }
+
+    /*
+    @Test
+    void saqueValorNuloTest() {
+        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> contaBancaria.saque(null));
+
+        assertEquals("Valor informado nao é valido", illegalArgumentException.getMessage());
+    }
+     */
+
+        @Test
+        void saqueValorNuloTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+
+            Executable valorNulo = () -> contaBancaria.saque(null);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,valorNulo);
+
+            assertEquals("Valor informado nao é valido", illegalArgumentException.getMessage());
+        }
+
+    /*
+    @Test
+    void saqueValorIgualAZeroTest() {
+        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> contaBancaria.saque(BigDecimal.ZERO));
+
+        assertEquals("Valor informado nao é valido", illegalArgumentException.getMessage());
+    }
+     */
+
+        @Test
+        void saqueValorIgualAZeroTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+
+            Executable chamdaAoMetodoComValorDeSaqueIgualAZero = () -> contaBancaria.saque(BigDecimal.ZERO);
+            IllegalArgumentException illegalArgumentException =
+                    assertThrows(IllegalArgumentException.class,chamdaAoMetodoComValorDeSaqueIgualAZero);
+
+            assertEquals("Valor informado nao é valido", illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void saqueValorMenorQueZeroTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.saque(BigDecimal.valueOf(-1)));
+
+            assertEquals("Valor informado nao é valido", illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void deveLancarExcecaoSaldoNull() {
+
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> new ContaBancaria(null));
+
+            assertEquals("Saldo esta nulo", illegalArgumentException.getMessage());
+
+        }
+    }
+
+    @Nested
+    class deposito {
+        @Test
+        void depositoComSucessoTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+            contaBancaria.deposito(BigDecimal.TEN);
+
+            assertEquals(BigDecimal.valueOf(60), contaBancaria.saldo());
+        }
+
+        @Test
+        void depositoValorNuloTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.deposito(null));
+
+            assertEquals("Valor informado nao é valido",  illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void depositoValorIgualAZeroTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.deposito(BigDecimal.ZERO));
+
+            assertEquals("Valor informado nao é valido",  illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void depositoValorMenorQueZeroTest() {
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.deposito(BigDecimal.valueOf(-1)));
+
+            assertEquals("Valor informado nao é valido",  illegalArgumentException.getMessage());
+        }
+    }
+
+
+
+    @Test
+    void deveRetornarSaldoTest() {
+        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+        assertEquals(BigDecimal.valueOf(50), contaBancaria.saldo());
+    }
+
+}
